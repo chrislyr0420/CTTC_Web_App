3618574508b4e9908a2f0027db34945fa860151c
chrislyr0420 <li.yiran@wustl.edu>
2020-12-05 01:12:00 +0800

    Modify local nginx to make reverse proxy
    now in my local desktop, I can hit http://localhost:3002 to reach to fontend page (http://localhost:3000) and in it's ajax request hit backend (http://localhost:3001/api/home)

--- react-cttc/src/component/Body.js
+++ react-cttc/src/component/Body.js
@@ -1,9 +1,15 @@
 import logo from '../asset/logo.svg';
 import React from 'react';
 
-function Body() {
-    return (
-        <body className="App-body">
+class Body extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            msg: ''
+        }
+    }
+    render () {
+        return <div className="App-body">
             <div className="body-top-wrapper">
                 <div className="body-logo-wrapper">
                     <img src={logo} className="App-logo" alt="logo" />
@@ -11,11 +17,30 @@ function Body() {
             </div>
             <div className="body-main-wrapper">
                 <div id="left-container" className="container">left content</div>
-                <div id="main-container" className="container">main content</div>
+                <div id="main-container" className="container">
+                    <div>{this.state.msg}</div>
+                </div>
                 <div id="right-container" className="container">right content</div>
             </div>
-        </body>
-    );
+        </div>
+    };
+
+    componentDidMount() {
+        fetch('/api/home', {
+            method: 'GET'
+        })
+            .then((response) => {
+                console.log(response);
+                return response.text();
+            })
+        .then((response) => {
+
+            this.setState({
+                msg: response
+            })
+        });
+
+    }
 }
 
 export default Body;
